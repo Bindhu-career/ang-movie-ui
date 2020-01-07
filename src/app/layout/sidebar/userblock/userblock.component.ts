965724ec9c2c5c39708f58251d2c02dd37c5a97d
Bindhu A <bindhu.career@gmail.com>
2020-01-07 12:24:39 +0530

    Angular movie project with multi module

---
+++ src/app/layout/sidebar/userblock/userblock.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { UserblockService } from './userblock.service';
+
+@Component({
+    selector: 'app-userblock',
+    templateUrl: './userblock.component.html',
+    styleUrls: ['./userblock.component.scss']
+})
+export class UserblockComponent implements OnInit {
+    user: any;
+    constructor(public userblockService: UserblockService) {
+        // this.user = {
+        //     picture: 'assets/img/user/03.jpg'
+        // };
+    }
+
+
+    ngOnInit() {
+        this.getUserDetails()
+    }
+
+    userBlockIsVisible() {
+        return this.userblockService.getVisibility();
+    }
+
+    getUserDetails() {
+        this.user = JSON.parse(localStorage.getItem('currentUser'));
+        if (this.user) {
+            this.user.picture='assets/img/user/03.jpg' 
+            this.userblockService.toggleVisibility(); 
+        }
+    }
+
+}
